4ad4a1e91f1c2cb20833e1be590e21af61409742
Dani Roxberry <dani@bitoriented.com>
2018-07-29 10:35:28 -0700

    [add] client

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,21 @@
 import React, { Component } from 'react';
-import {Button} from 'primereact/button';
-import logo from './logo.png';
+import logo from './logo.svg';
 import './App.css';
-import 'primereact/resources/themes/omega/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
 
 class App extends Component {
-    
-    constructor() {
-        super();
-        this.state = {count: 0};
-        this.increment = this.increment.bind(this);
-    }
-    
-    increment() {
-        this.setState({count: this.state.count + 1});
-    }
-    
-    render() {
-        return (
-          <div className="App">
-            <div className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h2>Welcome to PrimeReact</h2>
-            </div>
-            <div className="App-intro">
-              <Button label="Click" icon="pi pi-check" onClick={this.increment} />
-              
-              <p>Number of Clicks: {this.state.count}</p>
-            </div>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
